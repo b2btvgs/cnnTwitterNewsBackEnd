9a72cc3fcb1f16e4246f71b61fe5c021e45a5351
b2btvgs <paulmccranie@Pauls-Air.attlocal.net>
2015-10-04 16:30:41 -0400

    after twitter callback update

--- api/services/config.js
+++ api/services/config.js
@@ -2,6 +2,6 @@ module.exports = {
 
     TWITTER_KEY: '2riqzRQsA8aH9dGj75m7RP1BJ',
     TWITTER_SECRET: 'a5BAZaPTzHRIfOGI0VJNM8PlSMgYHtoe5p0dijTD0vQVIBXbpR',
-    TWITTER_CALLBACK: 'http://localhost:8000',
+    TWITTER_CALLBACK: 'http://45.55.247.9:8000/',
     TOKEN_SECRET: 'blahblah'
 };
